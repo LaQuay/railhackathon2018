c962f82ded4a6e4f39fdcfde4334b618eb0bc541
Marc Vila <marcvilagomez@gmail.com>
2018-10-06 10:48:37 +0200

    FIX caché Webview

--- android/app/src/main/java/com/hackathon/railmapchallenge/koalamap/MainActivity.java
+++ android/app/src/main/java/com/hackathon/railmapchallenge/koalamap/MainActivity.java
@@ -2,8 +2,10 @@ package com.hackathon.railmapchallenge.koalamap;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
 import android.view.Window;
 import android.view.WindowManager;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -23,11 +25,30 @@ public class MainActivity extends AppCompatActivity {
 
     private void loadWebView(String url) {
         webView = (WebView) findViewById(R.id.webView);
+        webView.clearCache(true);
+        webView.getSettings().setAppCacheEnabled(false);
+        webView.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
         webView.getSettings().setJavaScriptEnabled(true);
         webView.getSettings().setLoadWithOverviewMode(true);
         webView.getSettings().setUseWideViewPort(true);
         webView.setWebViewClient(new HelloWebViewClient());
         webView.loadUrl(url);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (event.getAction() == KeyEvent.ACTION_DOWN) {
+            switch (keyCode) {
+                case KeyEvent.KEYCODE_BACK:
+                    if (webView.canGoBack()) {
+                        webView.goBack();
+                    } else {
+                        finish();
+                    }
+                    return true;
+            }
+        }
+        return super.onKeyDown(keyCode, event);
     }
 
     private class HelloWebViewClient extends WebViewClient {
